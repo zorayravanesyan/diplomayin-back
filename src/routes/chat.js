4becf29d3c7a86b6da77d77cbe33136b29fa8427
zorayravanesyan <avanesyanzorik7@gmail.com>
2026-05-08 05:32:25 +0400

    aa

--- src/routes/chat.js
+++ src/routes/chat.js
@@ -5,26 +5,11 @@ const { validate, createConversationSchema, sendMessageSchema } = require('../ut
 
 const router = express.Router();
 
-router.post(
-  '/conversations',
-  authenticateToken,
-  validate(createConversationSchema),
-  chatController.createConversation
-);
+router.post('/conversations', authenticateToken, validate(createConversationSchema), chatController.createConversation);
 router.get('/conversations', authenticateToken, chatController.listConversations);
 router.get('/conversations/:id', authenticateToken, chatController.getConversation);
-router.post(
-  '/conversations/:id/messages/stream',
-  authenticateToken,
-  validate(sendMessageSchema),
-  chatController.sendMessageStream
-);
-router.post(
-  '/conversations/:id/messages',
-  authenticateToken,
-  validate(sendMessageSchema),
-  chatController.sendMessage
-);
+router.post('/conversations/:id/messages/stream', authenticateToken, validate(sendMessageSchema), chatController.sendMessageStream);
+router.post('/conversations/:id/messages', authenticateToken, validate(sendMessageSchema), chatController.sendMessage);
 router.delete('/conversations/:id', authenticateToken, chatController.deleteConversation);
 
 module.exports = router;
